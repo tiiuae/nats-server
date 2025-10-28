7950f85c8f189bb8081ad0745c08d8a47f1921cc
Kristian Mattila <55744103+thurrox@users.noreply.github.com>
2025-10-28 14:59:05 +0200

    Fix message dependency check.

--- server/stream.go
+++ server/stream.go
@@ -4747,10 +4747,11 @@ func (mset *stream) processJetStreamMsg(subject, reply string, hdr, msg []byte, 
 		ts = time.Now().UnixNano()
 	}
 
+	var msgDeps MessageDependencies
 	var dependenciesErr error = nil
 	if mset.cfg.CheckMessageDependencies {
 		mset.srv.Debugf("Checking message dependencies in stream '%s' for subject '%s', header '%s'", mset.cfg.Name, subject, string(hdr))
-		msgDeps, err := mset.getMessageDependencies(hdr)
+		msgDeps, err = mset.getMessageDependencies(hdr)
 		if err != nil {
 			mset.srv.Errorf("Failed to get message dependencies in stream '%s' for subject '%s': %v", mset.cfg.Name, subject, err)
 			// skip delay and treat as no dependencies, could be some unknown internal message
@@ -4780,6 +4781,7 @@ func (mset *stream) processJetStreamMsg(subject, reply string, hdr, msg []byte, 
 					subj:   subject,
 					hdr:    copyBytes(hdr),
 					msg:    copyBytes(msg),
+					deps:   msgDeps,
 				})
 
 				if len(mset.delayedMsgs) > mset.delayedMessagesSoftLimit {
