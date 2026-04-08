d8df66ca352c6276b5f99b867dab4f6eeb5cfe04
Jere Luomajoki <jere.luomajoki@solita.fi>
2026-04-08 11:26:31 +0300

    make $JS subject overlap check more lenient

--- server/stream.go
+++ server/stream.go
@@ -2024,14 +2024,15 @@ func (s *Server) checkStreamCfg(config *StreamConfig, acc *Account, pedantic boo
 			// Also check to make sure we do not overlap with our $JS API subjects.
 			if !cfg.NoAck {
 				for _, namespace := range []string{"$JS.>", "$JSC.>", "$NRG.>"} {
-					if SubjectsCollide(subj, namespace) {
+					// Changed from SubjectsCollide to subjectsIsSubsetMatch to be more lenient, allowing to sub to *.something.etc
+					if subjectIsSubsetMatch(subj, namespace) {
 						// We allow an exception for $JS.EVENT.> since these could have been created in the past.
 						if !subjectIsSubsetMatch(subj, "$JS.EVENT.>") {
 							return StreamConfig{}, NewJSStreamInvalidConfigError(fmt.Errorf("subjects that overlap with jetstream api require no-ack to be true"))
 						}
 					}
 				}
-				if SubjectsCollide(subj, "$SYS.>") {
+				if subjectIsSubsetMatch(subj, "$SYS.>") {
 					if !subjectIsSubsetMatch(subj, "$SYS.ACCOUNT.>") {
 						return StreamConfig{}, NewJSStreamInvalidConfigError(fmt.Errorf("subjects that overlap with system api require no-ack to be true"))
 					}
